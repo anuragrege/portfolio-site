d6d4c6745196a606d232264117bd82c6b1acfcab
anuragrege <anuragrege@gmail.com>
2023-11-04 18:19:02 +0530

    add academics section and minor fixes

--- src/App.js
+++ src/App.js
@@ -44,7 +44,10 @@ class App extends Component {
                 <a href="#skills">Skills</a>
               </li>
               <li>
-                <a href="#academic">Academics</a>
+                <a href="#acad">Academics</a>
+              </li>
+              <li>
+                <a href="#exp">Experience</a>
               </li>
               <li>
                 <a href="#contact">Contact</a>
@@ -191,6 +194,59 @@ class App extends Component {
             </div>
           </section>
 
+          <section id="acad" className="section">
+            <u>
+              <h2>Academics</h2>
+            </u>
+            <div className="academic-awards-section">
+              <div className="academic-section">
+                <div className="cgpa">
+                  <u>
+                    <h2>CGPA</h2>
+                  </u>
+                  <p>First Semester: 9.68</p>
+                  <p>Second Semester: 9.72</p>
+                  <p>Average CGPA: 9.70</p>
+                </div>
+              </div>
+              <div className="awards-section">
+                <div class="awards-list">
+                  <u>
+                    <h2>Awards/Achievements</h2>
+                  </u>
+                  <ul>
+                    <li>Outstanding Achievement Award- Standard 9</li>
+                    <li>Prefect of the Class in Standard 5,8,9</li>
+                    <li>House Captain in Standard 10</li>
+                    <li>Academic Performance Award- Standard 8</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="achievements-section">
+                <div class="achievements-list">
+                  <u>
+                    <h2>Standardized Exams</h2>
+                  </u>
+                  <ul>
+                    <li>Middle School and High School Scholarship Holder</li>
+                    <li>
+                      Ranked 2nd in District in MTSE examination in Standard 8
+                    </li>
+                    <li>
+                      3rd in school and 1st in boys in the school in 10th Board
+                      Exams
+                    </li>
+                    <li>97.80% in 10th boards with 100/100 in Sanskrit</li>
+                    <li>
+                      Highest Marks in Geography in 12th Board Examination in
+                      the District
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+
           <section id="exp" className="section">
             <u>
               <h2>Experience</h2>
@@ -237,8 +293,8 @@ class App extends Component {
                 <div className="experience-content">
                   <h2>Alexa Developers SRM</h2>
                   <p>
-                    Member of the Creative Team at a student club officially
-                    affiliated with Amazon Alexa
+                    Member of the Creative and Technical Team at a student club
+                    officially affiliated with Amazon Alexa
                   </p>
                   <p>NOVEMBER 2022-Present</p>
                   <a href="https://alexadevsrm.com/" target="blank">
@@ -255,7 +311,7 @@ class App extends Component {
                   <h2>WictroniX</h2>
                   <p>
                     Front end Developer Intern @WictroniX, a startup Company
-                    based in Suat that provides digital solutions to other
+                    based in Surat that provides digital solutions to other
                     companies
                   </p>
                   <p>AUGUST 2023-Present</p>
