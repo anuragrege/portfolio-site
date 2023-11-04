5ca633e6dd3ac7a82dc413cf27e7a0140e080365
anuragrege <anuragrege@gmail.com>
2023-11-05 01:30:48 +0530

    certifications and hackathons section

--- src/App.js
+++ src/App.js
@@ -382,6 +382,32 @@ class App extends Component {
             </div>
           </section>
 
+          <section id="cah" className="section">
+            <u>
+              <h2>Certifications and Hackathons</h2>
+            </u>
+            <h2>Certifications</h2>
+            <div className="certifications-section">
+              <div class="certification">
+                <img src="/assets/img/jscert.jpg" alt="Certificate 1" />
+                <p>Programing With Javascript</p>
+              </div>
+              <div class="certification">
+                <img src="/assets/img/iirs.jpg" alt="Certificate 2" />
+                <p>IIRS Course on Geospatial Analysis</p>
+              </div>
+            </div>
+
+            <div className="hackathons-section">
+              <h2>Hackathons</h2>
+              <br />
+              <ol>
+                <li>SMART INDIA Hackathon 2023- Finalists</li>
+                <li>OODP Hackathon SRMIST</li>
+                <li>LAUNCHHACKS II Hackathon</li>
+              </ol>
+            </div>
+          </section>
           <section id="contact" className="section">
             <u>
               <h2>Contact Me</h2>
