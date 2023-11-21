255ecdb308dc2e96bdb04271978c52b70bf4c316
anuragrege <anuragrege@gmail.com>
2023-11-21 16:06:38 +0530

    fix and animate certifications section

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const viewanimation = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 2 } },
+};
 
 const Certificates = () => {
   return (
@@ -6,27 +12,33 @@ const Certificates = () => {
       <u>
         <h2>Certifications and Hackathons</h2>
       </u>
-      <h2>Certifications</h2>
-      <div className="certifications-section">
-        <div class="certification">
-          <img src="/assets/img/jscert.jpg" alt="Certificate 1" />
-          <p>Programing With Javascript</p>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={viewanimation}
+      >
+        <h2>Certifications</h2>
+        <div className="certifications-section">
+          <div class="certification">
+            <img src="/assets/img/jscert.jpg" alt="Certificate 1" />
+            <p>Programing With Javascript</p>
+          </div>
+          <div class="certification">
+            <img src="/assets/img/iirs.jpg" alt="Certificate 2" />
+            <p>IIRS Course on Geospatial Analysis</p>
+          </div>
         </div>
-        <div class="certification">
-          <img src="/assets/img/iirs.jpg" alt="Certificate 2" />
-          <p>IIRS Course on Geospatial Analysis</p>
-        </div>
-      </div>
 
-      <div className="hackathons-section">
-        <h2>Hackathons</h2>
-        <br />
-        <ol>
-          <li>SMART INDIA Hackathon 2023- Finalists</li>
-          <li>OODP Hackathon SRMIST</li>
-          <li>LAUNCHHACKS II Hackathon</li>
-        </ol>
-      </div>
+        <div className="hackathons-section">
+          <h2>Hackathons</h2>
+          <br />
+          <ol>
+            <li>SMART INDIA Hackathon 2023- Finalists</li>
+            <li>OODP Hackathon SRMIST</li>
+            <li>LAUNCHHACKS II Hackathon</li>
+          </ol>
+        </div>
+      </motion.div>
     </section>
   );
 };
